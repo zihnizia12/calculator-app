25fbf2091702f267778a1504ebda9ffa670eca5c
zia <z.zia.pramono@students.amikom.ac.id>
2026-04-09 14:48:50 +0700

    melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,14 @@ class Calculator
 {
 	static void Main(string[] agrs)
 	{
+		int a = 10;
+		int b = 6;
 		
+		console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Penambahan(a, b));
+		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey)();
 	}
 	
 	static int Penambahan(int a, int b)
